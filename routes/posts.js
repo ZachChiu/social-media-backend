f681e3926e6fa51f3329ae8491b4e34ada6fee25
Zach <Zach>
2022-05-19 02:00:50 +0800

    feat: add isAuth middleware to post api

--- routes/posts.js
+++ routes/posts.js
@@ -1,17 +1,17 @@
 var express = require("express");
 var router = express.Router();
 const PostsController = require("../controllers/postsController");
-const handleErrorAsync = require("../server/handleErrorAsync");
+const { isAuth } = require("../server/auth");
 
-router.get("/", async function (req, res, next) {
+router.get("/", isAuth, async function (req, res, next) {
   PostsController.getPosts(req, res);
 });
 
-router.get("/:id", async function (req, res, next) {
+router.get("/:id", isAuth, async function (req, res, next) {
   PostsController.getPost(req, res, next);
 });
 
-router.post("/", async function (req, res, next) {
+router.post("/", isAuth, async function (req, res, next) {
   PostsController.createPost(req, res, next);
 });
 
@@ -19,11 +19,11 @@ router.delete("/", async function (req, res, next) {
   PostsController.deletePosts(req, res, next);
 });
 
-router.delete("/:id", async function (req, res, next) {
+router.delete("/:id", isAuth, async function (req, res, next) {
   PostsController.deletePost(req, res, next);
 });
 
-router.patch("/:id", async function (req, res, next) {
+router.patch("/:id", isAuth, async function (req, res, next) {
   PostsController.updatePost(req, res, next);
 });
 
